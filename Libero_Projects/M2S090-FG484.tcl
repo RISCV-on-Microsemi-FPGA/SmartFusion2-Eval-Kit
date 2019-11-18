3b32b0124258ad9281c3d90d3ffb3cd9b0193203
Sebastian <Seb.Slowikowski@microchip.com>
2019-11-18 10:32:15 +0000

    updated layout v1.0

--- Libero_Projects/M2S090-FG484.tcl
+++ Libero_Projects/M2S090-FG484.tcl
@@ -2,17 +2,17 @@ set project_folder_name_ahb MiV_RV32IMA_L1_AHB_BaseDesign
 set project_dir_ahb "./$project_folder_name_ahb"
 set Libero_project_name_ahb MiV_AHB_BaseDesign
 
-append Libero_project_name_ahb "_M2S090TS-FG484"
-append project_dir_ahb "_M2S090TS-FG484"
-append project_folder_name_ahb "_M2S090TS-FG484"
+append Libero_project_name_ahb "_M2S090-FG484"
+append project_dir_ahb "_M2S090-FG484"
+append project_folder_name_ahb "_M2S090-FG484"
 
 set project_folder_name_axi MiV_RV32IMA_L1_AXI_BaseDesign
 set project_dir_axi "./$project_folder_name_axi"
 set Libero_project_name_axi MiV_AXI_BaseDesign
 
-append Libero_project_name_axi "_M2S090TS-FG484"
-append project_dir_axi "_M2S090TS-FG484"
-append project_folder_name_axi "_M2S090TS-FG484"
+append Libero_project_name_axi "_M2S090-FG484"
+append project_dir_axi "_M2S090-FG484"
+append project_folder_name_axi "_M2S090-FG484"
 
 set target [string toupper [lindex $argv 0]]
 set design_flow_stage [string toupper [lindex $argv 1]]
@@ -22,7 +22,7 @@ proc create_new_project_label_ahb { }\
 	puts "-------------------------------------------------------------------------"
     puts "-------------------------------------------------------------------------"
     puts "-----------------------CREATING NEW PROJECT------------------------------"
-	puts "----------MiV_RV32IMA_L1_AHB_BaseDesign_M2S090TS-FG484-------------------"
+	puts "------------MiV_RV32IMA_L1_AHB_BaseDesign_M2S090-FG484-------------------"
     puts "-------------------------------------------------------------------------"
 	puts "-------------------------------------------------------------------------"
 }
@@ -32,7 +32,7 @@ proc create_new_project_label_axi { }\
 	puts "-------------------------------------------------------------------------"
     puts "-------------------------------------------------------------------------"
     puts "-----------------------CREATING NEW PROJECT------------------------------"
-	puts "----------MiV_RV32IMA_L1_AXI_BaseDesign_M2S090TS-FG484-------------------"
+	puts "------------MiV_RV32IMA_L1_AXI_BaseDesign_M2S090-FG484-------------------"
     puts "-------------------------------------------------------------------------"
 	puts "-------------------------------------------------------------------------"
 }
@@ -52,9 +52,9 @@ if {"$target" == "AHB"} then {
 		project_exists
 	} else {
 		create_new_project_label_ahb
-		new_project -location $project_dir_ahb -name $Libero_project_name_ahb -project_description {} -project_description {} -block_mode 0 -standalone_peripheral_initialization 0 -instantiate_in_smartdesign 1 -ondemand_build_dh 1 -hdl {VERILOG} -family {SmartFusion2} -die {M2S090TS} -package {484 FBGA} -speed {STD} -die_voltage {1.2} -part_range {COM} -adv_options {IO_DEFT_STD:LVCMOS 2.5V} -adv_options {RESTRICTPROBEPINS:1} -adv_options {RESTRICTSPIPINS:0} -adv_options {TEMPR:COM} -adv_options {UNUSED_MSS_IO_RESISTOR_PULL:None} -adv_options {VCCI_1.2_VOLTR:COM} -adv_options {VCCI_1.5_VOLTR:COM} -adv_options {VCCI_1.8_VOLTR:COM} -adv_options {VCCI_2.5_VOLTR:COM} -adv_options {VCCI_3.3_VOLTR:COM} -adv_options {VOLTR:COM} 
+		new_project -location $project_dir_ahb -name $Libero_project_name_ahb -project_description {} -project_description {} -block_mode 0 -standalone_peripheral_initialization 0 -instantiate_in_smartdesign 1 -ondemand_build_dh 1 -hdl {VERILOG} -family {SmartFusion2} -die {M2S090} -package {484 FBGA} -speed {STD} -die_voltage {1.2} -part_range {COM} -adv_options {IO_DEFT_STD:LVCMOS 2.5V} -adv_options {RESTRICTPROBEPINS:1} -adv_options {RESTRICTSPIPINS:0} -adv_options {TEMPR:COM} -adv_options {UNUSED_MSS_IO_RESISTOR_PULL:None} -adv_options {VCCI_1.2_VOLTR:COM} -adv_options {VCCI_1.5_VOLTR:COM} -adv_options {VCCI_1.8_VOLTR:COM} -adv_options {VCCI_2.5_VOLTR:COM} -adv_options {VCCI_3.3_VOLTR:COM} -adv_options {VOLTR:COM} 
 		file copy ./import/hex/  ./$project_folder_name_ahb
-		source ./import/components/M2S090TS-FG484/AHB/import_component_and_constraints_sf2_m2s090ts_ahb.tcl    
+		source ./import/components/M2S090-FG484/AHB/import_component_and_constraints_sf2_m2s090_ahb.tcl    
 		save_project
 		puts "-------------------------------------------------------------------------"
 		puts "-------------------------------SUCCESS!----------------------------------"
@@ -67,9 +67,9 @@ if {"$target" == "AHB"} then {
 		project_exists
 	} else {
 		create_new_project_label_axi
-		new_project -location $project_dir_axi -name $Libero_project_name_axi -project_description {} -project_description {} -block_mode 0 -standalone_peripheral_initialization 0 -instantiate_in_smartdesign 1 -ondemand_build_dh 1 -hdl {VERILOG} -family {SmartFusion2} -die {M2S090TS} -package {484 FBGA} -speed {STD} -die_voltage {1.2} -part_range {COM} -adv_options {IO_DEFT_STD:LVCMOS 2.5V} -adv_options {RESTRICTPROBEPINS:1} -adv_options {RESTRICTSPIPINS:0} -adv_options {TEMPR:COM} -adv_options {UNUSED_MSS_IO_RESISTOR_PULL:None} -adv_options {VCCI_1.2_VOLTR:COM} -adv_options {VCCI_1.5_VOLTR:COM} -adv_options {VCCI_1.8_VOLTR:COM} -adv_options {VCCI_2.5_VOLTR:COM} -adv_options {VCCI_3.3_VOLTR:COM} -adv_options {VOLTR:COM} 
+		new_project -location $project_dir_axi -name $Libero_project_name_axi -project_description {} -project_description {} -block_mode 0 -standalone_peripheral_initialization 0 -instantiate_in_smartdesign 1 -ondemand_build_dh 1 -hdl {VERILOG} -family {SmartFusion2} -die {M2S090} -package {484 FBGA} -speed {STD} -die_voltage {1.2} -part_range {COM} -adv_options {IO_DEFT_STD:LVCMOS 2.5V} -adv_options {RESTRICTPROBEPINS:1} -adv_options {RESTRICTSPIPINS:0} -adv_options {TEMPR:COM} -adv_options {UNUSED_MSS_IO_RESISTOR_PULL:None} -adv_options {VCCI_1.2_VOLTR:COM} -adv_options {VCCI_1.5_VOLTR:COM} -adv_options {VCCI_1.8_VOLTR:COM} -adv_options {VCCI_2.5_VOLTR:COM} -adv_options {VCCI_3.3_VOLTR:COM} -adv_options {VOLTR:COM} 
 		file copy ./import/hex/  ./$project_folder_name_axi
-		source ./import/components/M2S090TS-FG484/AXI/import_component_and_constraints_sf2_m2s090ts_axi.tcl 
+		source ./import/components/M2S090-FG484/AXI/import_component_and_constraints_sf2_m2s090_axi.tcl 
 		save_project
 		puts "-------------------------------------------------------------------------"
 		puts "-------------------------------SUCCESS!----------------------------------"
@@ -82,9 +82,9 @@ if {"$target" == "AHB"} then {
 		project_exists
 	} else {
 		create_new_project_label_ahb
-		new_project -location $project_dir_ahb -name $Libero_project_name_ahb -project_description {} -project_description {} -block_mode 0 -standalone_peripheral_initialization 0 -instantiate_in_smartdesign 1 -ondemand_build_dh 1 -hdl {VERILOG} -family {SmartFusion2} -die {M2S090TS} -package {484 FBGA} -speed {STD} -die_voltage {1.2} -part_range {COM} -adv_options {IO_DEFT_STD:LVCMOS 2.5V} -adv_options {RESTRICTPROBEPINS:1} -adv_options {RESTRICTSPIPINS:0} -adv_options {TEMPR:COM} -adv_options {UNUSED_MSS_IO_RESISTOR_PULL:None} -adv_options {VCCI_1.2_VOLTR:COM} -adv_options {VCCI_1.5_VOLTR:COM} -adv_options {VCCI_1.8_VOLTR:COM} -adv_options {VCCI_2.5_VOLTR:COM} -adv_options {VCCI_3.3_VOLTR:COM} -adv_options {VOLTR:COM} 
+		new_project -location $project_dir_ahb -name $Libero_project_name_ahb -project_description {} -project_description {} -block_mode 0 -standalone_peripheral_initialization 0 -instantiate_in_smartdesign 1 -ondemand_build_dh 1 -hdl {VERILOG} -family {SmartFusion2} -die {M2S090} -package {484 FBGA} -speed {STD} -die_voltage {1.2} -part_range {COM} -adv_options {IO_DEFT_STD:LVCMOS 2.5V} -adv_options {RESTRICTPROBEPINS:1} -adv_options {RESTRICTSPIPINS:0} -adv_options {TEMPR:COM} -adv_options {UNUSED_MSS_IO_RESISTOR_PULL:None} -adv_options {VCCI_1.2_VOLTR:COM} -adv_options {VCCI_1.5_VOLTR:COM} -adv_options {VCCI_1.8_VOLTR:COM} -adv_options {VCCI_2.5_VOLTR:COM} -adv_options {VCCI_3.3_VOLTR:COM} -adv_options {VOLTR:COM} 
 		file copy ./import/hex/  ./$project_folder_name_ahb
-		source ./import/components/M2S090TS-FG484/AHB/import_component_and_constraints_sf2_m2s090ts_ahb.tcl 
+		source ./import/components/M2S090-FG484/AHB/import_component_and_constraints_sf2_m2s090_ahb.tcl
 		save_project
 		puts "-------------------------------------------------------------------------"
 		puts "-------------------------------SUCCESS!----------------------------------"
@@ -107,11 +107,7 @@ if {"$design_flow_stage" == "SYNTHESIZE"} then {
     puts "--------------------------PLACE AND ROUTE--------------------------------"
     puts "-------------------------------------------------------------------------"
 	
-	configure_tool -name {PLACEROUTE} -params {TDPR:true} -params {IOREG_COMBINING:true} -params {INCRPLACEANDROUTE:false} -params {REPAIR_MIN_DELAY:true} -params {EFFORT_LEVEL:true}
-	get_tool_state -name {COMPILE} 
-	get_tool_state -name {SYNTHESIZE} 
-	get_flow_state 
-	configure_tool -name {PLACEROUTE} -params {START_SEED_INDEX:7} -params {RANDOM_SEED:82287664}
+	configure_tool -name {PLACEROUTE} -params {TDPR:true} -params {IOREG_COMBINING:true} -params {INCRPLACEANDROUTE:false} -params {REPAIR_MIN_DELAY:true} -params {EFFORT_LEVEL:true} -params {START_SEED_INDEX:7} -params {RANDOM_SEED:82287664}
 	run_tool -name {VERIFYTIMING}
 	save_project
 
@@ -121,12 +117,7 @@ if {"$design_flow_stage" == "SYNTHESIZE"} then {
     puts "--------------------GENERATING PROGRAMMING FILES-------------------------"
     puts "-------------------------------------------------------------------------"
 
-	configure_tool -name {PLACEROUTE} -params {TDPR:true} -params {IOREG_COMBINING:true} -params {INCRPLACEANDROUTE:false} -params {REPAIR_MIN_DELAY:true} -params {EFFORT_LEVEL:true}
-	get_tool_state -name {COMPILE} 
-	get_tool_state -name {SYNTHESIZE} 
-	get_flow_state 
-	configure_tool -name {PLACEROUTE} -params {START_SEED_INDEX:7} -params {RANDOM_SEED:82287664}
-	run_tool -name {VERIFYTIMING}
+	configure_tool -name {PLACEROUTE} -params {TDPR:true} -params {IOREG_COMBINING:true} -params {INCRPLACEANDROUTE:false} -params {REPAIR_MIN_DELAY:true} -params {EFFORT_LEVEL:true} -params {START_SEED_INDEX:7} -params {RANDOM_SEED:82287664}
     run_tool -name {GENERATEPROGRAMMINGDATA}
     run_tool -name {GENERATEPROGRAMMINGFILE}
     save_project
@@ -136,17 +127,14 @@ if {"$design_flow_stage" == "SYNTHESIZE"} then {
     puts "----------------------EXPORT PROGRAMMING FILES---------------------------"
     puts "-------------------------------------------------------------------------"
 
-	configure_tool -name {PLACEROUTE} -params {TDPR:true} -params {IOREG_COMBINING:true} -params {INCRPLACEANDROUTE:false} -params {REPAIR_MIN_DELAY:true} -params {EFFORT_LEVEL:true}
-	get_tool_state -name {COMPILE} 
-	get_tool_state -name {SYNTHESIZE} 
-	get_flow_state 
-	configure_tool -name {PLACEROUTE} -params {START_SEED_INDEX:7} -params {RANDOM_SEED:82287664}
-	run_tool -name {VERIFYTIMING}
+	configure_tool -name {PLACEROUTE} -params {TDPR:true} -params {IOREG_COMBINING:true} -params {INCRPLACEANDROUTE:false} -params {REPAIR_MIN_DELAY:true} -params {EFFORT_LEVEL:true} -params {START_SEED_INDEX:7} -params {RANDOM_SEED:82287664}
+    run_tool -name {GENERATEPROGRAMMINGDATA}
+    run_tool -name {GENERATEPROGRAMMINGFILE}
 
 	if {"$target" == "AHB"} then {
 		export_prog_job \
-			-job_file_name {BaseDesign_M2S090-AHB-ProgramFile} \
-			-export_dir {./FlashPro_Express_Projects} \
+			-job_file_name {BaseDesign_M2S090TS-AHB-ProgramFile} \
+			-export_dir {./../FlashPro_Express_Projects/Programming_Files} \
 			-bitstream_file_type {TRUSTED_FACILITY} \
 			-bitstream_file_components {FABRIC ENVM} \
 			-bitstream_file_components {}
@@ -154,8 +142,8 @@ if {"$design_flow_stage" == "SYNTHESIZE"} then {
 		
 	} else {
 		export_prog_job \
-			-job_file_name {BaseDesign_M2S090-AXI-ProgramFile} \
-			-export_dir {./FlashPro_Express_Projects} \
+			-job_file_name {BaseDesign_M2S090TS-AXI-ProgramFile} \
+			-export_dir {./../FlashPro_Express_Projects/Programming_Files} \
 			-bitstream_file_type {TRUSTED_FACILITY} \
 			-bitstream_file_components {FABRIC ENVM} \
 			-bitstream_file_components {}
