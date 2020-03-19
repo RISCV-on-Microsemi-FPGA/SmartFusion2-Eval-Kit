e0751bf2dd56013799933648723803083aac9db3
Sebastian <Seb.Slowikowski@microchip.com>
2020-03-19 12:01:48 +0000

    Updating the top level script.
    Top level script has been updated to improve user messaging. The export location for programming files has been changed to default location used by Libero so, that users don't overwrite the provided programming files in FlashPro_Express_Projects

--- Libero_Projects/M2S090-FG484.tcl
+++ Libero_Projects/M2S090-FG484.tcl
@@ -12,40 +12,47 @@ set design_flow_stage [string toupper [lindex $argv 1]]
 
 proc create_new_project_label_axi { }\
 {
-	puts "-------------------------------------------------------------------------"
-    puts "-------------------------------------------------------------------------"
-    puts "-----------------------CREATING NEW PROJECT------------------------------"
-	puts "------------MiV_RV32IMA_L1_AXI_BaseDesign_SF2_EVAL_KIT-------------------"
-    puts "-------------------------------------------------------------------------"
-	puts "-------------------------------------------------------------------------"
+   puts "-----------------------------------------------------------------------------------------------"
+   puts "-----------------------------------------------------------------------------------------------"
+   puts "---------------------------Creating New Project------------------------------------"
+   puts "------------SF2_EVAL_KIT_MiV_AXI_BaseDesign--------------------------"
+   puts "-----------------------------------------------------------------------------------------------"
+   puts "-----------------------------------------------------------------------------------------------"
 }
 
 proc create_new_project_label_ahb { }\
 {
-	puts "-------------------------------------------------------------------------"
-    puts "-------------------------------------------------------------------------"
-    puts "-----------------------CREATING NEW PROJECT------------------------------"
-	puts "------------MiV_RV32IMA_L1_AHB_BaseDesign_SF2_EVAL_KIT-------------------"
-    puts "-------------------------------------------------------------------------"
-	puts "-------------------------------------------------------------------------"
+   puts "-----------------------------------------------------------------------------------------------"
+   puts "-----------------------------------------------------------------------------------------------"
+   puts "---------------------------Creating New Project------------------------------------"
+   puts "------------SF2_EVAL_KIT_MiV_AHB_BaseDesign------------------------"
+   puts "-----------------------------------------------------------------------------------------------"
+   puts "-----------------------------------------------------------------------------------------------"
 }
 
 proc project_exists { }\
 {
-	puts "---------------------------------------------------------------------------"
-    puts "-------------------------------ERROR !-------------------------------------"
-	puts "---------------------------------------------------------------------------"
-    puts "Project already exists in folder. Please rename or remove and rerun script."
-	puts "---------------------------------------------------------------------------"
+   puts "-----------------------------------------------------------------------------------------------"
+   puts "----------------------------------------Error-----------------------------------------------"
+   puts "-----------------------------------------------------------------------------------------------"
+   puts "Project already exists in folder. Please rename or remove and rerun script."
+   puts "-----------------------------------------------------------------------------------------------"
 }
 
 proc invalid_argument { }\
 {
-	puts "---------------------------------------------------------------------------"
-    puts "----------------------WRONG ARGUMENT ENTERED !-----------------------------"
-	puts "---------------------------------------------------------------------------"
-    puts "Make sure you put AHB or AXI argument in front of the Design Flow argument!"
-	puts "---------------------------------------------------------------------------"
+   puts "-----------------------------------------------------------------------------------------------"
+   puts "----------------------Wrong Argument Entered ---------------------------------"
+   puts "-----------------------------------------------------------------------------------------------"
+   puts "Make sure you put AHB or AXI argument in front of the Design Flow argument."
+   puts "-----------------------------------------------------------------------------------------------"
+}
+
+proc  base_design_built {}\
+{
+  puts "-----------------------------------------------------------------------------------------------"
+  puts "------------------------------BaseDesign Built---------------------------------------"
+  puts "-----------------------------------------------------------------------------------------------"
 }
 
 proc  base_design_built {}\
@@ -97,172 +104,90 @@ if {"$target" == "AHB"} then {
 
 
 if {"$design_flow_stage" == "SYNTHESIZE"} then {
-	puts " "
-    puts "-------------------------------------------------------------------------"
-    puts "-----------------------------SYNTHESIS-----------------------------------"
-    puts "-------------------------------------------------------------------------"
-	puts " "
+    puts "-----------------------------------------------------------------------------------------------"
+    puts "-------------------------Begin Synthesis---------------------------------------------"
+    puts "-----------------------------------------------------------------------------------------------"
 
 	# Configuring Place_and_Route tool for a timing pass.
 	configure_tool -name {PLACEROUTE} -params {DELAY_ANALYSIS:MAX} -params {EFFORT_LEVEL:false} -params {INCRPLACEANDROUTE:false} -params {IOREG_COMBINING:true} -params {MULTI_PASS_CRITERIA:VIOLATIONS} -params {MULTI_PASS_LAYOUT:false} -params {NUM_MULTI_PASSES:5} -params {PDPR:false} -params {RANDOM_SEED:82287664} -params {REPAIR_MIN_DELAY:true} -params {SLACK_CRITERIA:WORST_SLACK} -params {SPECIFIC_CLOCK:} -params {START_SEED_INDEX:7} -params {STOP_ON_FIRST_PASS:false} -params {TDPR:true} 
-	# ##
+
     run_tool -name {SYNTHESIZE}
     save_project
 
-	puts " "
-    puts "-------------------------------------------------------------------------"
-    puts "---------------------------SYNTHESIZED!----------------------------------"
-    puts "-------------------------------------------------------------------------"
-	puts " "
+    puts "-----------------------------------------------------------------------------------------------"
+    puts "---------------------------Synthesis Complete-------------------------------------"
+    puts "-----------------------------------------------------------------------------------------------"
 	
 } elseif {"$design_flow_stage" == "PLACE_AND_ROUTE"} then {
-	puts " "
-    puts "-------------------------------------------------------------------------"
-    puts "--------------------------PLACE AND ROUTE--------------------------------"
-    puts "-------------------------------------------------------------------------"
-	puts " "
+
+    puts "-----------------------------------------------------------------------------------------------"
+    puts "----------------------Begin Place and Route--------------------------------------"
+    puts "-----------------------------------------------------------------------------------------------"
 	
 	# Configuring Place_and_Route tool for a timing pass.
 	configure_tool -name {PLACEROUTE} -params {DELAY_ANALYSIS:MAX} -params {EFFORT_LEVEL:false} -params {INCRPLACEANDROUTE:false} -params {IOREG_COMBINING:true} -params {MULTI_PASS_CRITERIA:VIOLATIONS} -params {MULTI_PASS_LAYOUT:false} -params {NUM_MULTI_PASSES:5} -params {PDPR:false} -params {RANDOM_SEED:82287664} -params {REPAIR_MIN_DELAY:true} -params {SLACK_CRITERIA:WORST_SLACK} -params {SPECIFIC_CLOCK:} -params {START_SEED_INDEX:7} -params {STOP_ON_FIRST_PASS:false} -params {TDPR:true} 
-	# ##
+	
 	run_tool -name {PLACEROUTE}
 	save_project
 	
-	puts " "
-    puts "-------------------------------------------------------------------------"
-    puts "----------------------DESIGN PLACED AND ROUTED!--------------------------"
-    puts "-------------------------------------------------------------------------"
-	puts " "
-
+    puts "-----------------------------------------------------------------------------------------------"
+    puts "----------------------Place and Route Complete--------------------------------"
+    puts "-----------------------------------------------------------------------------------------------"
 	
 } elseif {"$design_flow_stage" == "GENERATE_BITSTREAM"} then {
-	puts " "
-    puts "-------------------------------------------------------------------------"
-    puts "--------------------GENERATING PROGRAMMING FILES-------------------------"
-    puts "-------------------------------------------------------------------------"
-	puts " "
+
+    puts "-----------------------------------------------------------------------------------------------"
+    puts "----------------------------Generating Bitstream----------------------------------"
+    puts "-----------------------------------------------------------------------------------------------"
 
 	# Configuring Place_and_Route tool for a timing pass.
 	configure_tool -name {PLACEROUTE} -params {DELAY_ANALYSIS:MAX} -params {EFFORT_LEVEL:false} -params {INCRPLACEANDROUTE:false} -params {IOREG_COMBINING:true} -params {MULTI_PASS_CRITERIA:VIOLATIONS} -params {MULTI_PASS_LAYOUT:false} -params {NUM_MULTI_PASSES:5} -params {PDPR:false} -params {RANDOM_SEED:82287664} -params {REPAIR_MIN_DELAY:true} -params {SLACK_CRITERIA:WORST_SLACK} -params {SPECIFIC_CLOCK:} -params {START_SEED_INDEX:7} -params {STOP_ON_FIRST_PASS:false} -params {TDPR:true} 
-	# ##
+	
     run_tool -name {GENERATEPROGRAMMINGDATA}
     run_tool -name {GENERATEPROGRAMMINGFILE}
     save_project
 	
-	puts " "
-    puts "-------------------------------------------------------------------------"
-    puts "--------------------PROGRAMMING FILES GENERATED!-------------------------"
-    puts "-------------------------------------------------------------------------"
-	puts " "
-
+    puts "-----------------------------------------------------------------------------------------------"
+    puts "-------------------------------Bitstream Generated---------------------------------"
+    puts "-----------------------------------------------------------------------------------------------"
 
 } elseif {"$design_flow_stage" == "EXPORT_PROGRAMMING_FILE"} then {
-	puts " "
-    puts "-------------------------------------------------------------------------"
-    puts "----------------------EXPORT PROGRAMMING FILES---------------------------"
-    puts "-------------------------------------------------------------------------"
-	puts " "
 
-# Configuring Place_and_Route tool for a timing pass.
-configure_tool -name {PLACEROUTE} -params {DELAY_ANALYSIS:MAX} -params {EFFORT_LEVEL:false} -params {INCRPLACEANDROUTE:false} -params {IOREG_COMBINING:true} -params {MULTI_PASS_CRITERIA:VIOLATIONS} -params {MULTI_PASS_LAYOUT:false} -params {NUM_MULTI_PASSES:5} -params {PDPR:false} -params {RANDOM_SEED:82287664} -params {REPAIR_MIN_DELAY:true} -params {SLACK_CRITERIA:WORST_SLACK} -params {SPECIFIC_CLOCK:} -params {START_SEED_INDEX:7} -params {STOP_ON_FIRST_PASS:false} -params {TDPR:true} 
-# ##
-# pre-requisite to enable exporting programming file function
-    run_tool -name {GENERATEPROGRAMMINGDATA}
-    run_tool -name {GENERATEPROGRAMMINGFILE}
-# ##
+    puts "-----------------------------------------------------------------------------------------------"
+    puts "----------------------Exporting Programming Files----------------------------"
+    puts "-----------------------------------------------------------------------------------------------"
+
+	# Configuring Place_and_Route tool for a timing pass.
+	configure_tool -name {PLACEROUTE} -params {DELAY_ANALYSIS:MAX} -params {EFFORT_LEVEL:false} -params {INCRPLACEANDROUTE:false} -params {IOREG_COMBINING:true} -params {MULTI_PASS_CRITERIA:VIOLATIONS} -params {MULTI_PASS_LAYOUT:false} -params {NUM_MULTI_PASSES:5} -params {PDPR:false} -params {RANDOM_SEED:82287664} -params {REPAIR_MIN_DELAY:true} -params {SLACK_CRITERIA:WORST_SLACK} -params {SPECIFIC_CLOCK:} -params {START_SEED_INDEX:7} -params {STOP_ON_FIRST_PASS:false} -params {TDPR:true} 
+
+	# pre-requisite to enable exporting programming file function
+		run_tool -name {GENERATEPROGRAMMINGDATA}
+		run_tool -name {GENERATEPROGRAMMINGFILE}
 
 	if {"$target" == "AHB"} then {
 		export_prog_job \
-			-job_file_name {BaseDesign_SF2_Eval_Kit_AHB_ProgramFile} \
-			-export_dir {./../FlashPro_Express_Projects/Programming_Files} \
+			-job_file_name {SF2_EVAL_KIT_MiV_AHB_BaseDesign} \
+			-export_dir {./MiV_AHB_BD/designer/BaseDesign/export} \
 			-bitstream_file_type {TRUSTED_FACILITY} \
 			-bitstream_file_components {}
 		save_project
 		
 	} else {
 		export_prog_job \
-			-job_file_name {BaseDesign_SF2_Eval_Kit_AXI_ProgramFile} \
-			-export_dir {./../FlashPro_Express_Projects/Programming_Files} \
+			-job_file_name {SF2_EVAL_KIT_MiV_AXI_BaseDesign} \
+			-export_dir {./MiV_AXI_BD/designer/BaseDesign/export} \
 			-bitstream_file_type {TRUSTED_FACILITY} \
 			-bitstream_file_components {}
 		save_project
 	}
 	
-	puts " "
-    puts "-------------------------------------------------------------------------"
-    puts "--------------------PROGRAMMING FILES EXPORTED!--------------------------"
-    puts "-------------------------------------------------------------------------"
-	puts " "
+    puts "-----------------------------------------------------------------------------------------------"
+    puts "--------------------Programming Files Exported-------------------------------"
+    puts "-----------------------------------------------------------------------------------------------"
 
 } else {
+    puts "-----------------------------------------------------------------------------------------------"
+    puts "------------No Design Flow  Stage Selected at run time------------------"
+    puts "-----------------------------------------------------------------------------------------------"   
 
-	if {"$target" == "AHB"} then {
-
-		puts " "
-		puts " "
-		puts " "
-		puts " "
-		puts "-------------------------------------------------------------------------"
-		puts "-------------------------------------------------------------------------"
-		puts "-----------------NO VALID DESIGN FLOW ARGUMENT ENTERED!------------------"
-		puts "--------------------------(this is optional)-----------------------------"
-		puts "-------------------------------------------------------------------------"
-		puts " "
-		puts " "
-		puts " "
-		puts " "
-		puts " "
-		puts " "
-		puts " "
-		puts " "
-		puts " "
-		puts "-------------------------------------------------------------------------"
-		puts "-------------------------------------------------------------------------"
-		puts "-------------------------SCRIPT EXECUTED!--------------------------------"
-		puts "-------------------------------------------------------------------------"
-		puts "-------------------------------------------------------------------------"
-
-	
-	} elseif {"$target" == "AXI"} then {
-	
-		puts " "
-		puts " "
-		puts " "
-		puts " "
-		puts "-------------------------------------------------------------------------"
-		puts "-------------------------------------------------------------------------"
-		puts "-----------------NO VALID DESIGN FLOW ARGUMENT ENTERED!------------------"
-		puts "--------------------------(this is optional)-----------------------------"
-		puts "-------------------------------------------------------------------------"
-		puts " "
-		puts " "
-		puts " "
-		puts " "
-		puts " "
-		puts " "
-		puts " "
-		puts " "
-		puts " "
-		puts "-------------------------------------------------------------------------"
-		puts "-------------------------------------------------------------------------"
-		puts "-------------------------SCRIPT EXECUTED!--------------------------------"
-		puts "-------------------------------------------------------------------------"
-		puts "-------------------------------------------------------------------------"
-	
-	} else {
-		puts " "
-		puts " "
-		puts " "
-		puts " "
-		puts "-------------------------------------------------------------------------"
-		puts "-------------------------------------------------------------------------"
-		puts "-----------------NO VALID DESIGN FLOW ARGUMENT ENTERED!------------------"
-		puts "--------------------------(this is optional)-----------------------------"
-		puts "-------------------------------------------------------------------------"
-		puts "-------ENTER A VALID FIRST ARGUMENT TO USE DESIGN FLOW ARGUMENT----------"
-		puts "------------------------------AHB OR AXI---------------------------------"
-		puts "-------------------------------------------------------------------------"
-		puts "-------------------------------------------------------------------------"
-
-	}
 }
